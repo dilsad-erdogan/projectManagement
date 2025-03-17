e08c28aee0bb0dbf02ff274bfce1d82f769f960a
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-18 02:13:26 +0300

    router

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux"
+import store from "../redux/store";
+import Navbar from "../components/Navbar";
+import Main from "../pages/Main";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Provider store={store}>
+      <div className='h-screen'>
+        <Router>
+          <Navbar />
+
+          <div className='flex w-full overflow-hidden'>
+            <Toaster position='top-right' />
+
+            <Routes>
+              <Route path='/' element={<Main />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </Provider>
   )
 }
