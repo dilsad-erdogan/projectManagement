f7f68d605247cf246f66f5805b44c8073358425d
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-20 08:32:33 +0300

    jobs

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import JobCard from "../components/JobCard";
 
 const Main = () => {
+  const jobs = useSelector(state => state.job.jobs);
+  console.log(jobs);
+
   return (
-    <div>Main</div>
+    <div className='flex-1 justify-center items-center mt-16'>
+      <div className='flex flex-col p-2 gap-10'>
+        {jobs.map((job) => (
+          <div key={job._id} className="bg-white w-full p-4 rounded-lg shadow-md">
+            <JobCard job={job} />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
