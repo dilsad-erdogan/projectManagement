65218a4cf3c2b1ea96adb540736f531569fc52ac
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-04-22 18:42:11 +0300

    job card

--- client/src/app/Main.jsx
+++ client/src/app/Main.jsx
@@ -8,9 +8,9 @@ const Main = () => {
 
   return (
     <div className='flex-1 justify-center items-center mt-16'>
-      <div className='flex flex-col p-2 gap-10'>
+      <div className='flex flex-col p-2 gap-5'>
         {jobs.map((job) => (
-          <div key={job._id} className="bg-white w-full p-4 rounded-lg shadow-md">
+          <div key={job._id} className="bg-white w-full p-4 rounded-lg shadow-md transition duration-200 transform hover:scale-y-125 cursor-pointer">
             <JobCard job={job} />
           </div>
         ))}
