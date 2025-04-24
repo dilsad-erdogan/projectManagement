678122290da13857f110fbbe861b5c715efb99e8
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-04-24 22:54:11 +0300

    customer jobs

---
+++ client/src/pages/Customer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Customer = () => {
+  return (
+    <div className="flex justify-center mt-10">
+        job customer
+    </div>
+  )
+}
+
+export default Customer
