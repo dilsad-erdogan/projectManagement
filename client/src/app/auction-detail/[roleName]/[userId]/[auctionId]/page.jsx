07deb9aaed1c48c2bf75c64d3aca08f0d131608b
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-04-28 17:02:34 +0300

    sections

--- client/src/app/auction-detail/[roleName]/[userId]/[auctionId]/page.jsx
+++ client/src/app/auction-detail/[roleName]/[userId]/[auctionId]/page.jsx
@@ -1,10 +1,27 @@
 "use client";
 
-import React from 'react'
+import React from 'react';
+import { useParams } from 'next/navigation';
+import Sidebar from '@/components/Sidebar';
 
 const page = () => {
+  const params = useParams();
+  const { roleName, userId, auctionId } = params;
+
   return (
-    <div>page</div>
+    <div className='flex justify-center items-start gap-10 w-full max-w-[1200px] mt-2'>
+      {/* Sidebar */}
+      <div className="w-1/6 p-5 mt-10 h-full overflow-y-auto border-r border-black dark:border-white">
+        <Sidebar roleName={roleName} userId={userId} />
+      </div>
+
+      {/* Content */}
+      <div className='flex justify-center items-start w-full max-w-[1200px]'>
+        <div className="w-5/6 p-5 flex flex-col justify-start overflow-y-auto">
+          {auctionId}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/app/job-detail/[roleName]/[userId]/[jobId]/page.jsx
+++ client/src/app/job-detail/[roleName]/[userId]/[jobId]/page.jsx
@@ -6,13 +6,20 @@ import Sidebar from '@/components/Sidebar';
 
 const page = () => {
     const params = useParams();
-    const { jobId } = params;
+    const { roleName, userId, jobId } = params;
 
     return (
         <div className='flex justify-center items-start gap-10 w-full max-w-[1200px] mt-2'>
             {/* Sidebar */}
             <div className="w-1/6 p-5 mt-10 h-full overflow-y-auto border-r border-black dark:border-white">
                 <Sidebar roleName={roleName} userId={userId} />
+            </div>
+
+            {/* Content */}
+            <div className='flex justify-center items-start w-full max-w-[1200px]'>
+                <div className="w-5/6 p-5 flex flex-col justify-start overflow-y-auto">
+                    {jobId}
+                </div>
             </div>
         </div>
     )
