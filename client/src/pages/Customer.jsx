e24045ccacb64878ca60ed49cdde88d3f9de995b
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-05-14 21:31:22 +0300

    open job button

--- client/src/pages/Customer.jsx
+++ client/src/pages/Customer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
 
-const Customer = () => {
+import React from 'react'
+import { useRouter } from 'next/navigation';
+
+const Customer = ({ roleName, userId }) => {
+  const router = useRouter();
+
   return (
-    <div className="flex justify-center mt-10">
-        job customer
+    <div className="flex flex-col gap-10 items-start justify-start mt-10 max-w-3xl mx-auto">
+      <button onClick={() => router.push(`/open-job/${roleName}/${userId}`)} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">Open Job</button>
     </div>
   )
 }
